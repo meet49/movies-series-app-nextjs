0fdb45181280793a2a464eab0817f4bb7fb18e58
meet49 <meetsojitra49@gmail.com>
2024-04-09 16:57:14 +0530

    movie series app using next js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,21 +1,57 @@
-import React from 'react'
-import Link from 'next/link'
-
+"use client"
+import React, { useState } from 'react';
+import Link from 'next/link';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
-      <nav className=" flex justify-between bg-blue-600">
-        <div className=" text-white text-3xl font-bold p-4 ml-16 sm:ml-20 md:ml-24 lg:ml-28">
+      <nav className="flex justify-between bg-blue-600">
+        <div className="text-white text-3xl font-bold p-4 ml-16 sm:ml-20 md:ml-24 lg:ml-28">
           <Link href="/">
             <span>DEMO Streaming</span>
           </Link>
         </div>
-        <div className="mr-8 flex gap-10 pr-6 text-white mt-5 flex-col sm:flex sm:flex-row">
+
+
+        {menuOpen ? (
+          <div className="mr-8 flex items-center sm:hidden">
+            <button onClick={toggleMenu} className="text-white focus:outline-none">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
+            </button>
+          </div>
+        ) : (
+          <div className={`mr-8 flex items-center ${menuOpen ? 'hidden' : 'block'} sm:hidden`}>
+            <button onClick={toggleMenu} className="text-white focus:outline-none">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              </svg>
+            </button>
+          </div>
+        )}
+
+        <div className={`gap-10 pr-6 text-white mt-5 flex-col sm:flex sm:flex-row ${menuOpen ? 'block' : 'hidden'}`}>
           <div className="p-2">
-            <Link href="/">
-              Log in
-            </Link>
+            <Link href="/">Log in</Link>
           </div>
           <button className="bg-slate-900 mb-3 p-2 w- flex items-center justify-center flex-wrap">
             Start your free trial
@@ -23,7 +59,7 @@ function Header() {
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
